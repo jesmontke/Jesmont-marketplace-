faa410c4c976e957c841edf085bd7722d6f37e15
jesmontke <tharikijohn713@gmail.com>
2025-06-06 09:59:13 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlakKgMzhADOywIOg4iTCJ5sUFXLMGwVg",
+  authDomain: "jesmont-marketplace.firebaseapp.com",
+  projectId: "jesmont-marketplace",
+  storageBucket: "jesmont-marketplace.appspot.com",
+  messagingSenderId: "543717950238",
+  appId: "1:543717950238:web:df009d49e88a2ea010bf0f",
+  measurementId: "G-56TMB41PS8"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export function protectPage(redirectUrl = 'login.html') {
+  onAuthStateChanged(auth, user => {
+    if (!user) {
+      alert("You must be logged in to view this page.");
+      window.location.href = redirectUrl;
+    }
+  });
+}
