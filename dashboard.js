5c1a514f9322d49b0fb459fcedb75791f8eb0d56
jesmontke <tharikijohn713@gmail.com>
2025-06-08 00:13:45 +0300

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -173,7 +173,7 @@ async function displayProducts() {
   const q = query(
     collection(db, "products"),
     where("sellerId", "==", currentUser.uid),
-    orderBy("createdAt", "desc")
+    ("createdAt", "desc")
   );
   const querySnapshot = await getDocs(q);
 
